cbea9e05d4418816361b55b23995cf8df04e3794
Kiran babu <51410158+RouthKiranbabu@users.noreply.github.com>
2020-04-01 18:45:22 +0530

    Difficulty: Easy.

---
+++ Polar Cordinated - HackerRank/Solution.py
@@ -0,0 +1,8 @@
+#Question Link: https://www.hackerrank.com/challenges/polar-coordinates/problem?isFullScreen=false&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+from cmath import phase
+
+Complex_Number = 1 + 2j
+
+print(abs(complex(Complex_Number)))
+
+print(phase(complex(Complex_Number)))
