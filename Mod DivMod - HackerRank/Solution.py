f8061af9f475de629401868f00ca984b726f7d2b
Kiran babu <51410158+RouthKiranbabu@users.noreply.github.com>
2020-04-02 15:39:17 +0530

    Difficulty: Easy.

---
+++ Mod DivMod - HackerRank/Solution.py
@@ -0,0 +1,6 @@
+#Question Link: https://www.hackerrank.com/challenges/python-mod-divmod/problem?h_r=next-challenge&h_v=zen
+a = int(input())
+b = int(input())
+print(a//b)
+print(a%b)
+print(divmod(a, b))
